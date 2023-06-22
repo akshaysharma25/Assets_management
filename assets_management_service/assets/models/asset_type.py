aba1b65073b45deae19479e6330721d7a14486d1
Mohit <mohit.gehilot@techademy.net>
2023-06-22 15:40:41 +0530

    Added API's: 1.Assets Dropdown 2.Renew Asset Request 3.Return Asset

--- assets_management_service/assets/models/asset_type.py
+++ assets_management_service/assets/models/asset_type.py
@@ -5,8 +5,17 @@ from assets.models.base import BaseModel
 class AssetTypeModel(BaseModel):
     asset_type = models.CharField(max_length=45, null=True, blank=False, unique=True)
 
+    def __str__(self):
+        return self.asset_type
+
 
 class AssetNameModel(BaseModel):
     asset_type = models.ForeignKey(AssetTypeModel, blank=False, on_delete=models.CASCADE, related_name="asset_name")
     asset_name = models.CharField(max_length=45, blank=False, null=True)
     description = models.CharField(max_length=255, blank=False, null=True)
+
+
+    def __str__(self):
+        return self.asset_name
+
+
